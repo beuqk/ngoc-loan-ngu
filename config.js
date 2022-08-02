5ff50dd35fdb58624c389d1ed67df53928f29463
beuqk <96179328+beuqk@users.noreply.github.com>
2022-08-02 12:50:20 +0700

    ngoc-loan-ngu

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐🖐🖐 Hee loo ! 🖐🖐🖐',
+    introDesc: `Mình có điều này muốn hỏi,cậu nhớ phải trả lời thật lòng nha`,
+    btnIntro: 'OK',
+    title: 'Cậu có bị thiểu năng không ? 😙',
+    desc: 'Không trả lời mà thoát ra tức là có :> ',
+    btnYes: 'Có, mình bị thiểu năng 💖💖💖',
+    btnNo: '💔 KHÔNG ! 💔 ',
+    question:'Có thật là cậu bị thiểu năng không vậy 😊😊',
+    reasonPlaceholder: 'Trả lời',
+    btnReply: 'Gởi ! 💌💌',
+    reply: 'Mình bị thiểu năng thật mà :',
+    mess: 'Mình biết mà , đúng là chỉ thiểu năng mới bấm zo 🥰. Love you 300.000 😘😘',
+    messDesc: '💬💬 Nhắn tin cho Thành Anh nha. 💬💬',
+    btnAccept: 'Ok lun 💖',
+    messLink: 'https://www.facebook.com/profile.php?id=100005254575586'
+}
